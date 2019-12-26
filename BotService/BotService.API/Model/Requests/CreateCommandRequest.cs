d819d1dfff67373193190d88c50685a9c2b77666
Sergey <seri05@yandex.ru>
2019-12-26 23:18:05 +0300

    implemented CreateCommandRequest

---
+++ BotService/BotService.API/Model/Requests/CreateCommandRequest.cs
@@ -0,0 +1,9 @@
+namespace BotService.API.Model
+{
+    public class CreateCommandRequest
+    {
+        public string Request { get; set; }
+        public string Response { get; set; }
+        public string BotName { get; set; }
+    }
+}
