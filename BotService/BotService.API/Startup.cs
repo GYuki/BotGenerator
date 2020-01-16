00e277e5311d037a4ba6f3d8f75d5298dbe8d221
Sergey <seri05@yandex.ru>
2020-01-17 00:40:41 +0300

    using mvc newtonsoftjson because of net core 3.0(referenceloophandling)

--- BotService/BotService.API/Startup.cs
+++ BotService/BotService.API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -40,6 +41,9 @@ namespace BotService.API
             services.AddTransient<ICommandRepository, SqlCommandRepository>();
             services.AddTransient<ISubscribeRepository, SqlSubscribeRepository>();
             services.AddControllers();
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                    .AddNewtonsoftJson(option => option.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
