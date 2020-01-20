34b02caa1b42f3b98d497517c86195021329ad5c
Sergey <seri05@yandex.ru>
2020-01-21 01:53:29 +0300

    implemented chat model

---
+++ TelegramReceiver/TelegramReceiver.API/Models/Chat.cs
@@ -0,0 +1,10 @@
+namespace TelegramReceiver.API.Models
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Username { get; set; }
+    }
+}
