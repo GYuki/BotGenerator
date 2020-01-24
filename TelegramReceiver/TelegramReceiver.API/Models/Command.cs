0d3ffcdf5b5211c222287e151d356495a6a46cca
Sergey <seri05@yandex.ru>
2020-01-24 06:54:29 +0300

    modified model for proper serialization

--- TelegramReceiver/TelegramReceiver.API/Models/Command.cs
+++ TelegramReceiver/TelegramReceiver.API/Models/Command.cs
@@ -1,12 +1,21 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
 namespace TelegramReceiver.API.Models
 {
+    [JsonObject(MemberSerialization.OptIn, NamingStrategyType = typeof(SnakeCaseNamingStrategy))]
     public class Command
     {
         public int Id { get; set; }
+        [JsonProperty(Required = Required.Always)]
         public string Token { get; set; }
+        [JsonProperty(Required = Required.Always)]
         public string Request { get; set; }
-        public int ResponseId { get; set; }
+
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Description { get; set; }
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
         public string Response { get; set; }
     }
 }
