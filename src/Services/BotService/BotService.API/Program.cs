43d353ce2677c31dc51517b6ec37802a71c0c3c4
Sergey <seri05@yandex.ru>
2020-02-04 00:49:09 +0300

    using custom buildwebhost

--- src/Services/BotService/BotService.API/Program.cs
+++ src/Services/BotService/BotService.API/Program.cs
@@ -1,9 +1,13 @@
 using System;
+using System.IO;
+using System.Net;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,10 +17,31 @@ namespace BotService.API
     {
         public static readonly string Namespace = typeof(Program).Namespace;
         public static readonly string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configuration = GetConfiguration();
+            // CreateHostBuilder(args).Build().Run();
+
+            var host = BuildWebHost(configuration, args);
+            host.Run();
+            return 0;
         }
+
+        private static IWebHost BuildWebHost(IConfiguration configuration, string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .CaptureStartupErrors(false)
+                .ConfigureKestrel(options =>
+                {
+                    var port = GetDefinedPorts(configuration);
+                    options.Listen(IPAddress.Any, port, listenOptions =>
+                    {
+                        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                    });
+                })
+                .UseStartup<Startup>()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseConfiguration(configuration)
+                .Build();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
@@ -24,5 +49,21 @@ namespace BotService.API
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        
+        private static IConfiguration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder.Build();
+        }
+        
+        private static int GetDefinedPorts(IConfiguration config)
+        {
+            var port = config.GetValue("PORT", 80);
+            return port;
+        }
     }
 }
