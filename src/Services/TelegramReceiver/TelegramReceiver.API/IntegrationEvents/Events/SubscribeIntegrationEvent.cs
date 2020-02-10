2067f31ca1778fdd632b985d6d9a500413ecae6b
Sergey <seri05@yandex.ru>
2020-02-11 00:24:57 +0300

    changed chatid type

--- src/Services/TelegramReceiver/TelegramReceiver.API/IntegrationEvents/Events/SubscribeIntegrationEvent.cs
+++ src/Services/TelegramReceiver/TelegramReceiver.API/IntegrationEvents/Events/SubscribeIntegrationEvent.cs
@@ -6,9 +6,9 @@ namespace TelegramReceiver.API.IntegrationEvents.Events
     public class SubscribeIntegrationEvent : IntegrationEvent
     {
         public string BotToken { get; private set; }
-        public string ChatId { get; private set; }
+        public int ChatId { get; private set; }
 
-        public SubscribeIntegrationEvent(string botToken, string chatId)
+        public SubscribeIntegrationEvent(string botToken, int chatId)
         {
             BotToken = botToken;
             ChatId = chatId;
