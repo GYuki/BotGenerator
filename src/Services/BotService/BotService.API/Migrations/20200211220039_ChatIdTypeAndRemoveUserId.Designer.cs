3346f0a47f4dd07be2e14ad1205cc9268d39812e
Sergey <seri05@yandex.ru>
2020-02-12 01:06:05 +0300

    migrations for both tables

--- src/Services/BotService/BotService.API/Migrations/20200211220039_ChatIdTypeAndRemoveUserId.Designer.cs
+++ src/Services/BotService/BotService.API/Migrations/20200211220039_ChatIdTypeAndRemoveUserId.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using BotService.API.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BotService.API.Migrations
 {
     [DbContext(typeof(BotContext))]
-    partial class BotContextModelSnapshot : ModelSnapshot
+    [Migration("20200211220039_ChatIdTypeAndRemoveUserId")]
+    partial class ChatIdTypeAndRemoveUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
